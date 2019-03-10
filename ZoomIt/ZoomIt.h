5f49241712d593eb9bdd53bf98f9b4696f1fcc0e
SherifZekryWebsites <prosherifzekry@gmail.com>
2019-03-10 11:05:02 +0200

    Initial Commit

---
+++ ZoomIt/ZoomIt.h
@@ -0,0 +1,19 @@
+//
+//  ZoomIt.h
+//  ZoomIt
+//
+//  Created by Sherif  Wagih on 3/10/19.
+//  Copyright © 2019 Sherif  Wagih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZoomIt.
+FOUNDATION_EXPORT double ZoomItVersionNumber;
+
+//! Project version string for ZoomIt.
+FOUNDATION_EXPORT const unsigned char ZoomItVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomIt/PublicHeader.h>
+
+
